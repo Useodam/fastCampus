0e3ff42c619376155b77091a65193906dd536f71
Useodam <db970212@gmail.com>
2024-03-20 18:59:08 +0900

    ResponseEntity, objectMapper

--- src/main/java/com/example/restapi/controller/ResponseApiController.java
+++ src/main/java/com/example/restapi/controller/ResponseApiController.java
@@ -2,7 +2,6 @@ package com.example.restapi.controller;
 
 import com.example.restapi.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
