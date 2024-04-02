046a4255fb222ce44d77bf7b2e0f44668c8a0c0f
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2024-04-02 19:07:13 +0900

    Update BookRepository.java
    JPA로 변환

--- 04. memorydb/demo/src/main/java/com/example/demo/book/db/repository/BookRepository.java
+++ 04. memorydb/demo/src/main/java/com/example/demo/book/db/repository/BookRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.book.db.repository;
 
 import com.example.demo.book.db.entity.BookEntity;
-import com.example.demo.db.SimpleDataRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class BookRepository extends SimpleDataRepository<BookEntity, Long> {
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
